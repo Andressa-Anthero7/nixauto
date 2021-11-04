9c6918b383041ce08413d87ec23f58605ca31812
Andressa <andressa.anthero7@gmail.com>
2021-11-04 18:20:01 -0300

    nix007

--- nixauto/settings.py
+++ nixauto/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-5vg%tbncbqpw91b0(@-#a27)vq)l_1%9&_8+wd0x&01b@b%mch
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','devnixauto.pythonanywhere.com']
+ALLOWED_HOSTS = ['devnixauto.pythonanywhere.com']
 
 
 # Application definition
